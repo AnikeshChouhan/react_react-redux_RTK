9d2b43b956102290bf7a7c60208159185f666d83
Anikesh Chouhan <anikeshmaheshwari558@gmail.com>
2025-03-06 18:41:53 +0530

    In direct Slice we cannt increse point when amount in above 100 but by using createReducer builder.addCase its posible

--- src/reducers/reward.js
+++ src/reducers/reward.js
@@ -2,6 +2,7 @@ import { createAction, createReducer } from "@reduxjs/toolkit";
 
 export const increment = createAction("reward/increment");
 export const incrementByAmount = createAction("reward/incrementByAmount");
+export const incByAmount = createAction("account/incrementByAmount");
 
 const initialState = { points: 0 };
 
@@ -12,5 +13,10 @@ export const rewardReducer = createReducer(initialState, (builder) => {
     })
     .addCase(incrementByAmount, (state, action) => {
       state.points += action.payload;
+    })
+    .addCase(incByAmount, (state, action) => {
+      if (action.payload >= 100) {
+        state.points++;
+      }
     });
 });
